5a8fc8f10f70f2896cfb6eac17f0c33f175bce8c
Gabriel R. Abreu <grsabreu@gmail.com>
2018-01-20 00:36:14 -0200

    Fix script

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+GH_PAGES_BRANCH=gh-pages
+
 TMP_DIR=$(mktemp -d)
 
 echo "\033[1;33m What have you done to the site?\033[0m"
@@ -16,7 +18,7 @@ BUILD_FILES=`ls ./build`
 cd ..
 
 # Wipe out current site
-git checkout master
+git checkout $GH_PAGES_BRANCH
 git rm -rf .
 mv .git $TMP_DIR/.git
 rm -rf .
@@ -30,7 +32,7 @@ git add $BUILD_FILES
 # Commit and push changes
 git commit -m "$update"
 echo "\033[1;31mPushing new site to GitHub repo\n\033[0m"
-git push --force origin master
+git push --force origin $GH_PAGES_BRANCH
 
 # Get back to develop
 git checkout develop
