6e19ac26772f005bb814bdf3ae92069ed74563c0
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2022-07-05 19:23:54 -0300

    chore(docs): added reform helpers

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   docs: [
     {
       Overview: ["what-and-why", "installation", "getting-started/getting-started"],
-      "API Reference": ["api/reform-make", "api/reform-use", "api/reform"]
+      "API Reference": ["api/reform-make", "api/reform-use", "api/reform", "api/reform-helpers"]
     },
   ],
 };
